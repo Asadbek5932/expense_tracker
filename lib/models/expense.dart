0a9c68d983c88d7f8b11701f3bfa1f7650be0257
macbookpro <asadbekakmlov435@gmail.com>
2024-04-05 11:05:19 +0500

    already ready

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,11 +1,33 @@
 import "package:uuid/uuid.dart";
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+const uuid = Uuid();
+
+enum Category { food, leisure, travel, work }
+
+final formatter = DateFormat.yMd();
+
+const icons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.work: Icons.work,
+  Category.leisure: Icons.movie
+};
 
 class Expense {
 
-  Expense( {required this.title, required this.amount, required this.date});
+  Expense(
+      {required this.title, required this.amount, required this.date, required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
-  final String amount;
+  final double amount;
   final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+class NewExpense extends StatefulWidget {
+  NewExpense(this.finishEnteringCategory, {Key? key}) : super(key: key);
+
+  final void Function(Expense expense) finishEnteringCategory;
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _textController = TextEditingController();
+  final _numberController = TextEditingController();
+  Category _selectedCategory = Category.leisure;
+  DateTime? date;
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _numberController.dispose();
+    super.dispose();
+  }
+
+  void _saveNewExpense() {
+    final enteredNumber = double.tryParse(_numberController.text);
+    if (_textController.text.trim().isEmpty ||
+        enteredNumber == null ||
+        date == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Invalid input'),
+          content: Text('Make sure that all user inputs are correct.'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Ok'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+    Expense expense = Expense(
+        title: _textController.text,
+        amount: double.tryParse(_numberController.text)!,
+        date: date!,
+        category: _selectedCategory);
+    widget.finishEnteringCategory(expense);
+    Navigator.pop(context);
+  }
+
+  void _showCalendar() async {
+    var firstDate = DateTime.now();
+    var lastDate = DateTime(firstDate.year - 1, firstDate.month, firstDate.day);
+    var lastDate2 =
+        DateTime(firstDate.year + 10, firstDate.month, firstDate.day);
+    final result = await showDatePicker(
+      context: context,
+      initialDate: firstDate,
+      firstDate: lastDate,
+      lastDate: lastDate2,
+    );
+    setState(() {
+      date = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        children: [
+          TextField(
+            controller: _textController,
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _numberController,
+                  decoration: InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '\$ ',
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      date == null ? 'Selected date' : formatter.format(date!),
+                    ),
+                    IconButton(
+                      onPressed: _showCalendar,
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (value) => DropdownMenuItem(
+                        value: value,
+                        child: Text(value.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              ElevatedButton(
+                onPressed: _saveNewExpense,
+                child: Text('Save Expense'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
